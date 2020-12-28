418f828ebc0c1ce85734c30e285ff841b06e753f
AlanPeter1 <35610163+AlanPeter1@users.noreply.github.com>
2020-12-28 16:26:16 +0000

    Update forms.py

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-#new page
 class NewEntryForm(forms.Form):
     title = forms.CharField(label="Title",required = True,help_text="<p class='text-secondary'>Help:<a class='text-info' href = https://docs.github.com/en/github/writing-on-github/basic-writing-and-formatting-syntax> GitHub’s Markdown guide</a> </p>",
     widget= forms.TextInput
@@ -10,7 +9,6 @@ class NewEntryForm(forms.Form):
     widget= forms.Textarea
     (attrs={'placeholder':'Enter markdown content','class':'col-sm-11','style':'top:0rem'}))
 
-#edit page
 class EditForm(forms.Form):
     title = forms.CharField(label="Title",required = True,help_text="<p class='text-secondary'>Help:<a class='text-info' href = https://docs.github.com/en/github/writing-on-github/basic-writing-and-formatting-syntax> GitHub’s Markdown guide</a> </p>",
     widget= forms.TextInput
